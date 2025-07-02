501d690a3acada44c4b2ae830b506352d5ec2a22
Stanley Delva <juniordelva1@gmail.com>
2025-07-01 23:58:38 -0400

    added an .env file and radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+const url = `https://api.apiflash.com/v1/urltoimage
+?access_key=${ACCESS_KEY}
+&url=https%3A%2F%2Fwww.greenmangaming.co...
+&format=jpeg
+&width=1920
+&height=1080
+&full_page=true`;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [inputs, setInputs] = useState({
+        url: "",
+        format: "",
+        width: "",
+        height: "",
+        full_page: "",
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function handleSubmit(e) {
+        e.preventDefault();
+        // if (!inputs.name) return;
+        console.log(inputs);
+        setInputs({
+            url: "",
+            format: "",
+            width: "",
+            height: "",
+            full_page: "",
+        });
+    }
+
+    function handleInputs(e) {
+        const { name, value } = e.target;
+        setInputs((prev) => ({ ...prev, [name]: value }));
+    }
+
+    return (
+        <>
+            <div className="form-div">
+                <form action="" onSubmit={handleSubmit}>
+                    <label htmlFor="">
+                        Enter The url{" "}
+                        <input
+                            type="text"
+                            name="url"
+                            value={inputs.url}
+                            onChange={handleInputs}
+                        />
+                    </label>
+
+                    <div className="radio-div">
+                        <p>Format</p>
+                        <label htmlFor="">
+                            <input
+                                type="radio"
+                                name="format"
+                                value="jpeg"
+                                checked={inputs.format === "jpeg"}
+                                onChange={handleInputs}
+                            />
+                            jpeg
+                        </label>
+                        <label htmlFor="">
+                            <input
+                                type="radio"
+                                name="format"
+                                value="png"
+                                checked={inputs.format === "png"}
+                                onChange={handleInputs}
+                            />
+                            png
+                        </label>
+                        <label htmlFor="">
+                            <input
+                                type="radio"
+                                name="format"
+                                value="webp"
+                                checked={inputs.format === "webp"}
+                                onChange={handleInputs}
+                            />
+                            webp
+                        </label>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
